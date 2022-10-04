6ff8e24e9608b8439c6ab80d6c1a650b67af05fe
Oh Ga eun <dhrkdms4113@naver.com>
2022-10-04 21:48:13 +0900

    리액트 기초

---
+++ src/commentList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Comment from "../src/comment";
+
+const comments = [
+    {
+        name: "ㅇㅇ",
+        comment : "ㅎㅇ",
+    },
+    {
+        name: "ㅇㅇ",
+        comment : "ㅎㅇ",
+    },
+    {
+        name: "ㅇㅇ",
+        comment : "ㅎㅇ",
+    },
+]
+
+function CommentList(props) {
+    return (
+        <div>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                )
+            })}
+        </div>
+    )
+} 
+
+export default CommentList;
