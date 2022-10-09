6d15827b4275dcbe2477bd2c21f8badfd0032aff
Oh Ga eun <dhrkdms4113@naver.com>
2022-10-09 20:26:37 +0900

    index, app 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 
+import Clock from './exampleCode/clock';
+import CommentList from "./exampleCode/commentList";
+import NotificationList from './exampleCode/notificationList';
+import Accommodate from './exampleCode/accommodate';
+import ConfirmButton from "./exampleCode/confirmButton";
+import LandingPage from './exampleCode/LandingPage';
+
 function App() {
   return (
-    <div></div>
+    <React.StrictMode>
+      <LandingPage />
+      <Accommodate />
+      <CommentList />
+      <NotificationList />
+      <ConfirmButton />
+    </React.StrictMode>
   );
 }
+
+// setInterval(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>
+//   );
+// })
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import Clock from './exampleCode/clock';
-import CommentList from "./exampleCode/commentList";
-import NotificationList from './exampleCode/notificationList';
-import Accommodate from './exampleCode/accommodate';
-import ConfirmButton from "./exampleCode/confirmButton";
-import LandingPage from './exampleCode/LandingPage';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-      <LandingPage />
-      <Accommodate />
-      <CommentList />
-      <NotificationList />
-      <ConfirmButton />
+      <App />
     </React.StrictMode>
 );
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-
-// import Clock from './clock';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// setInterval(() => {
-//   root.render(
-//     <React.StrictMode>
-//       <Clock />
-//     </React.StrictMode>
-//   );
-// })
