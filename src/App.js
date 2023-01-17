51b3afafc67e31462c3dfd4fecd9fb5e4645dcfd
Oh Ga Eun <dhrkdms4113@naver.com>
2023-01-17 14:47:46 +0900

    feat: expense record prj

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-function App() {
+import React from "react";
+import ExpenseItem from "./React완벽가이드/components/Expenses/Expenses"
+import NewExpense from "./React완벽가이드/components/newExpense/NewExpense";
+
+const App = () => {
+  const expense = [
+    {
+      id: 1,
+      title: 'Toilet Paper1',
+      amount: 94.12,
+      date: new Date(),
+    },
+    {
+      id: 2,
+      title: 'Toilet Paper2',
+      amount: 94.12,
+      date: new Date(),
+    },
+    {
+      id: 3,
+      title: 'Toilet Paper3',
+      amount: 94.12,
+      date: new Date(),
+    },
+    {
+      id: 4,
+      title: 'Toilet Paper4',
+      amount: 94.12,
+      date: new Date(),
+    },
+  ];
+
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <ExpenseItem item={expense} />
     </div>
   );
 }
