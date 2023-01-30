815d6f056915d192e1f686e1e2cc893ec4a298cf
Oh Ga Eun <dhrkdms4113@naver.com>
2023-01-30 15:39:45 +0900

    feat: user input components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import AddUser from "./UserInputComponents/Users/AddUser";
+import UserList from "./UserInputComponents/Users/UserList";
+
+function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      return [...prevUserList, {name: uName, age: uAge, id: Math.random().toString()}];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
+    </div>
+  );
+}
+
+export default App;
