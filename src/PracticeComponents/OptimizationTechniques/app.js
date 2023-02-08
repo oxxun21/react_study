7a87f1de9595f8fba359493af62962f135f35b20
Oh Ga Eun <dhrkdms4113@naver.com>
2023-02-08 12:54:30 +0900

    change file location

---
+++ src/PracticeComponents/OptimizationTechniques/app.js
@@ -0,0 +1,33 @@
+import React, { useCallback, useState } from 'react';
+
+import './App.css';
+import Button from './Button/Button'
+import DemoOutput from './Demo/DemoOutput';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log('App running');
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
