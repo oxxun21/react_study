16230753fc2716f800ff48ae8ebdd7145f5f8124
Oh Ga Eun <dhrkdms4113@naver.com>
2023-02-14 16:57:18 +0900

    order button implementation, backend interlocking

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -31,6 +31,7 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount
     };
   }
+
   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
     const existingItem = state.items[existingCartItemIndex];
@@ -50,6 +51,10 @@ const cartReducer = (state, action) => {
     }
   }
 
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -64,11 +69,16 @@ const CartProvider = (props) => {
     dispatchCartAction({type: 'REMOVE', id: id});
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({type: 'CLEAR'});
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
